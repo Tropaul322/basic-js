6b20891de7b129f5ff1c3487dfdd8bccef499523
Ymbi <60569456+Tropaul322@users.noreply.github.com>
2020-03-22 20:26:13 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  var name = [];
+  if (members === null || members === undefined){
+      return false
+  }
+  if (typeof members[0] != 'string' && typeof members[0] != 'object' ){
+      return false
+  }
+  for (var i = 0; i < members.length; i++){
+      if (typeof members[i] === 'string' ){
+      members[i] = members[i].replace(/ /g,'');
+      name.push(members[i][0].toUpperCase());
+      name.sort()
+       var b = name.join('')
+      }
+      }
+      return b
 };
