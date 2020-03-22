f930ea7735c64c23e6052b6d9d5bb206a7e2f745
Ymbi <60569456+Tropaul322@users.noreply.github.com>
2020-03-22 22:07:19 +0300

    Update what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,36 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date = 'aaa') {
+  if (date == 'aaa'){
+    return 'Unable to determine the time of year!';
+  }
+  if ( typeof date.getTime !='function'){
+    throw 'Error';
+  }
+  var regExp = /[a-z]+\s[a-z]+\s\d+\s(\d+)\s/i;
+    if(Number(date.toString().match(regExp)[1]) != date.getFullYear()){
+        throw 'Error';
+    }
+  var month = date.getMonth();
+  switch(month){
+    case 11:
+    case 0:
+    case 1:
+        return 'winter';
+    break;
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+    break;
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+    break;
+    case 8:
+    case 9:
+    case 10:
+      return 'autumn';
+      break;
+  }
+  
 };
