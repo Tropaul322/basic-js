bd9f2df45669b9899e230b954970ad6333c010bc
Ymbi <60569456+Tropaul322@users.noreply.github.com>
2020-03-22 22:10:40 +0300

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,53 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(!Array.isArray(arr)) {
+        throw 'Error';
+    }
+    if(arr == []) {
+        return []
+    }
+
+    var newArr = [];
+    var actions = [
+        '--discard-next',
+        '--discard-prev', 
+        '--double-next',
+        '--double-prev'
+    ];
+    var skipNext = false;
+    for ( var i =0; i<arr.length; i++) {
+        if(skipNext) {
+            skipNext = false;
+            continue;
+        }
+        if(arr[i] == actions[0]) {
+            if(i+1 == arr.length) {continue};
+            if(actions.includes(arr[i+1])) {
+                continue
+            } 
+            skipNext = true;
+        } 
+        if (arr[i] == actions[1]) {
+            if(i == 0) { continue};
+            if(actions.includes(arr[i-1])) {
+                continue;
+            } 
+            newArr.pop();
+        }
+        if (arr[i] == actions[2]) {
+            if(i+1 == arr.length) {continue};
+            if(actions.includes(arr[i+1])) {
+                continue;
+            } 
+            newArr.push(arr[i+1])
+        }
+        if (arr[i] == actions[3]) {
+            if(i == 0) { continue};
+            if(actions.includes(arr[i-1])) {
+                continue;
+            } 
+            newArr.push(arr[i-1])
+        }
+        if(!actions.includes(arr[i])) {newArr.push(arr[i])};
+    }
+    return newArr;
 };
